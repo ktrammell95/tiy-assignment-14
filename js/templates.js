9a21c64a83fc63c47e52c9f3347f466e7873206d
Katherine Trammell <ktrammell@gmail.com>
2015-02-20 08:58:44 -0500

    Added templates

--- js/templates.js
+++ js/templates.js
@@ -1,4 +1,31 @@
 this["JST"] = this["JST"] || {};
-this["JST"]["photogroups"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-  return "";
+this["JST"]["ind-photos"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+  var helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
+  return "<div class=\"photo-full\">\n  <a href=\"#\">Back to Albums</a>\n  <div class=\"photo\">\n    <img src=\""
+    + escapeExpression(((helper = (helper = helpers.photo || (depth0 != null ? depth0.photo : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"photo","hash":{},"data":data}) : helper)))
+    + "\" alt=\""
+    + escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"name","hash":{},"data":data}) : helper)))
+    + "\">\n  </div>\n</div>";
+},"useData":true});
+this["JST"] = this["JST"] || {};
+this["JST"]["photo-albums"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+  var helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
+  return "<div class=\"photo-preview\">\n  <div class=\"photo\">\n    <img width=\"100\" src=\""
+    + escapeExpression(((helper = (helper = helpers.photo || (depth0 != null ? depth0.photo : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"photo","hash":{},"data":data}) : helper)))
+    + "\" alt name=\""
+    + escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"name","hash":{},"data":data}) : helper)))
+    + "\">\n  </div>\n  <div class=\"info\">\n    <strong>Name:</strong>\n    <span>"
+    + escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"name","hash":{},"data":data}) : helper)))
+    + "</span>\n  </div>\n  <div class=\"more\">\n    <a href=\"#\" image-detail-id="
+    + escapeExpression(((helper = (helper = helpers['image-detail'] || (depth0 != null ? depth0['image-detail'] : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"image-detail","hash":{},"data":data}) : helper)))
+    + ">\n    See Image</a>\n  </div>\n</div>";
+},"useData":true});
+this["JST"] = this["JST"] || {};
+this["JST"]["photo-groups"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+  var helper, functionType="function", helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
+  return "<li data-group-name=\""
+    + escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"name","hash":{},"data":data}) : helper)))
+    + "\">"
+    + escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"name","hash":{},"data":data}) : helper)))
+    + "</li>\n";
 },"useData":true});
